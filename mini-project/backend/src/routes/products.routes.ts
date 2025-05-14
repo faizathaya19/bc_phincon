1d421fde7cc77f708aec71710f14dc3aae941273
Faiz Athaya Ramadhan <faiz.ramadhan@phincon.com>
2025-05-14 14:09:23 +0700

    Fix .gitignore to only include my-folder

--- mini-project/backend/src/routes/products.routes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { product, products } from '../controllers/product.controller'
-
-const router = express.Router()
-
-router.get('/all', products)
-router.post('/', product)
-
-export default router
